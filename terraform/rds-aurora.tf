89ef7bef5a4cd0a95dc76e885bd73b1592bf170d
Thomas Barros Camara <thomasbcamara@gmail.com>
2022-03-07 18:14:10 +0100

    Initial terraform infra (ApiGtw, SQS, Lambada, Dynamo, RDS)

--- terraform/rds-aurora.tf
+++ terraform/rds-aurora.tf
@@ -1,26 +1,26 @@
-resource "aws_rds_cluster" "cluster" {
-  engine                  = "aurora-postgresql"
-  engine_mode             = "provisioned"
-  availability_zones      = ["eu-central-1a", "eu-central-1b", "eu-central-1c"]
-  backup_retention_period = 5
-  preferred_backup_window = "04:00-05:00"
-
-  cluster_identifier      = "reporting-incidents-aurora-cluster"
-  database_name           = "postgres"
-  master_username         = "root"
-  master_password         = "ZB2ys9D44Nj*54+g"
-}
-
-resource "aws_rds_cluster_instance" "cluster_instances" {
-  identifier         = "reporting-incidents-aurora-${count.index}"
-  count              = 1
-  cluster_identifier = aws_rds_cluster.cluster.id
-  instance_class     = "db.t3.medium"
-  engine             = aws_rds_cluster.cluster.engine
-  engine_version     = aws_rds_cluster.cluster.engine_version
-
-  publicly_accessible = true
-}
+#resource "aws_rds_cluster" "cluster" {
+#  engine                  = "aurora-postgresql"
+#  engine_mode             = "provisioned"
+#  availability_zones      = ["eu-central-1a", "eu-central-1b", "eu-central-1c"]
+#  backup_retention_period = 5
+#  preferred_backup_window = "04:00-05:00"
+#
+#  cluster_identifier      = "reporting-incidents-aurora-cluster"
+#  database_name           = "postgres"
+#  master_username         = "root"
+#  master_password         = "ZB2ys9D44Nj*54+g"
+#}
+#
+#resource "aws_rds_cluster_instance" "cluster_instances" {
+#  identifier         = "reporting-incidents-aurora-${count.index}"
+#  count              = 1
+#  cluster_identifier = aws_rds_cluster.cluster.id
+#  instance_class     = "db.t3.medium"
+#  engine             = aws_rds_cluster.cluster.engine
+#  engine_version     = aws_rds_cluster.cluster.engine_version
+#
+#  publicly_accessible = true
+#}
 
 #TODO:
 #TODO: create database proxy on Lambda configuration
