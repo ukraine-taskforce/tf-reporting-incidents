0abb539c762e05ab2f90b3c8b56dadb2f5b30737
Thomas Barros Camara <thomasbcamara@gmail.com>
2022-03-08 18:51:04 +0100

    Create dead letter queue in reporting incident flow.

--- terraform/sqs.tf
+++ terraform/sqs.tf
@@ -2,6 +2,15 @@ resource "aws_sqs_queue" "reporting-incidents_sqs" {
   name                      = "reporting-incidents-queue"
   delay_seconds             = 0
   receive_wait_time_seconds = 10
+
+  redrive_policy = jsonencode({
+    deadLetterTargetArn = aws_sqs_queue.reporting-incidents_sqs-dlq.arn
+    maxReceiveCount     = 5
+  })
 }
 
-#TODO: Create dead letter queue
+resource "aws_sqs_queue" "reporting-incidents_sqs-dlq" {
+  name                      = "reporting-incidents-dlq"
+  delay_seconds             = 0
+  receive_wait_time_seconds = 10
+}
