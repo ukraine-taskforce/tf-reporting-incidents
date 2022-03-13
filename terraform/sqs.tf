a6ab07e12ba3a101071df7ecf4a54858e24ca4b5
thomascamara <thomasbcamara@gmail.com>
2022-03-13 10:22:50 +0100

    Enable SQS encryiption and grant kms access to roles (#10)

--- terraform/sqs.tf
+++ terraform/sqs.tf
@@ -2,6 +2,7 @@ resource "aws_sqs_queue" "reporting-incidents_sqs" {
   name                      = "reporting-incidents-queue"
   delay_seconds             = 0
   receive_wait_time_seconds = 10
+  kms_master_key_id = aws_kms_alias.sqs-key-alias.name
 
   redrive_policy = jsonencode({
     deadLetterTargetArn = aws_sqs_queue.reporting-incidents_sqs-dlq.arn
@@ -13,4 +14,5 @@ resource "aws_sqs_queue" "reporting-incidents_sqs-dlq" {
   name                      = "reporting-incidents-dlq"
   delay_seconds             = 0
   receive_wait_time_seconds = 10
+  kms_master_key_id = aws_kms_alias.sqs-key-alias.name
 }
