8468263d87ea35d6ed1781304cd2eb5cde9290fb
Volodymyr <vovaminiof@gmail.com>
2022-03-15 12:40:28 +0100

    Update the flow to ask the time details (#12)
    * Update the flow to ask the time details
    
    * Update conversation handler to operate with state instead
    
    * Remove not needed formatting
    
    * Handle incorrect prompts
    
    Additionally refactored conversation handler to only attach reply markup to the last message.

--- lambdas/bot-client/main.py
+++ lambdas/bot-client/main.py
@@ -16,7 +16,14 @@ logger = logging.getLogger(__name__)
 
 bot = init_bot()
 
-START, INCIDENT, DISTANCE, END = "start", "incident", "distance", "end"
+START, INCIDENT, DISTANCE, TIME, END, ERROR = "start", "incident", "distance", "time", "end", "error"
+CONV_STATE_TO_CATEGORY = {
+    ConversationState.START: START,
+    ConversationState.LOCATION: START,
+    ConversationState.CATEGORY: INCIDENT,
+    ConversationState.LOCATION_DETAILS: DISTANCE,
+    ConversationState.TIME: TIME
+}
 
 
 class ConversationHandler:
@@ -28,41 +35,45 @@ class ConversationHandler:
         self.conversation = json.loads(open("conversation.json").read())
         logger.info(self.conversation)
 
-    def __list_messages(self, category, **condition_parameters):
-        for message in self.conversation[category]:
+    def __list_messages(self, category, state=None, reply_markup_category=None):
+        result = []
+        if state is None:
+            state = {}
+
+        for message in self.conversation[category]["message"]:
             text = None
             for conditional_text in message.get("conditional_text", []):
-                if eval(conditional_text["condition"] % condition_parameters):
+                if eval(conditional_text["condition"].format(state=state)):
                     text = conditional_text["text"].get(self.language_code, conditional_text["text"]["en"])
                     break
             if text is None:
                 text = message["text"].get(self.language_code, message["text"]["en"])
-            if "reply_markup" not in message:
-                yield text, {}
-            else:
-                markup = ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
-                for button in message["reply_markup"]:
-                    markup.add(KeyboardButton(button["text"].get(self.language_code, button["text"]["en"]),
-                                              **button.get("kwargs", {})))
 
-                yield text, {"reply_markup": markup}
+            result.append((text, {}))
+
+        reply_markup_block = self.conversation[reply_markup_category or category]
+        if "reply_markup" not in reply_markup_block:
+            return result
+
+        markup = ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
+        for button in reply_markup_block["reply_markup"]:
+            if "condition" in button and not eval(button["condition"].format(state=state)):
+                continue
+
+            markup.add(KeyboardButton(button["text"].get(self.language_code, button["text"]["en"]),
+                                      **button.get("kwargs", {})))
+
+        result.append((result.pop()[0], {"reply_markup": markup}))
+        return result
 
     def __get_button_id(self, category, button_text):
-        for message in self.conversation[category]:
-            for button in message.get("reply_markup", []):
-                if button["text"].get(self.language_code, button["text"]["en"]) == button_text:
-                    return button["id"]
-
-    def __get_button_text(self, category, button_id):
-        for message in self.conversation[category]:
-            for button in message.get("reply_markup", []):
-                if button["id"] == button_id:
-                    return button["text"].get(self.language_code, button["text"]["en"])
+        for button in self.conversation[category].get("reply_markup", []):
+            if button["text"].get(self.language_code, button["text"]["en"]) == button_text:
+                return button["id"]
 
     def get_reply_options(self, category):
-        for message in self.conversation[category]:
-            for button in message.get("reply_markup", []):
-                yield button["text"].get(self.language_code, button["text"]["en"])
+        for button in self.conversation[category].get("reply_markup", []):
+            yield button["text"].get(self.language_code, button["text"]["en"])
 
     def start(self):
         for message_text, kwargs in self.__list_messages(START):
@@ -71,34 +82,49 @@ class ConversationHandler:
         delete_state(self.user_id)
 
     def location(self):
-        for message_text, kwargs in self.__list_messages(INCIDENT):
-            bot.send_message(self.chat_id, message_text, **kwargs)
-
         state = {"user": {"language_code": self.language_code, "id": str(self.user_id)},
                  "incident": {
                      "location": {
                          "lat": self.message.location.latitude, "lon": self.message.location.longitude},
                      "timestamp": datetime.now().replace(microsecond=0).isoformat()}}
+
+        for message_text, kwargs in self.__list_messages(INCIDENT, state):
+            bot.send_message(self.chat_id, message_text, **kwargs)
+
         set_state(self.user_id, ConversationState.CATEGORY, state)
 
     def category(self, state):
         selected_category = self.__get_button_id(INCIDENT, self.message.text)
         state["incident"]["type"] = selected_category
 
-        for message_text, kwargs in self.__list_messages(DISTANCE, incident_id=selected_category):
-            bot.send_message(self.chat_id, message_text % {INCIDENT: self.message.text.lower()}, **kwargs)
+        for message_text, kwargs in self.__list_messages(DISTANCE, state):
+            bot.send_message(self.chat_id, message_text, **kwargs)
 
         update_state(self.user_id, ConversationState.LOCATION_DETAILS, state)
 
     def process_location_details(self, state):
         state["incident"]["distance"] = self.__get_button_id(DISTANCE, self.message.text)
 
-        for message_text, kwargs in self.__list_messages(END):
+        for message_text, kwargs in self.__list_messages(TIME, state):
             bot.send_message(self.chat_id, message_text, **kwargs)
+
+        update_state(self.user_id, ConversationState.TIME, state)
+
+    def process_time_details(self, state):
+        state["incident"]["time"] = self.__get_button_id(TIME, self.message.text)
+
+        for message_text, kwargs in self.__list_messages(END, state):
+            bot.send_message(self.chat_id, message_text, **kwargs)
+
         self.start()
 
         response = send_report(state)
         logger.info(f"SQS Response: {response}")
+
+    def process_unknown_prompt(self, conv_state: ConversationState, state):
+        for message_text, kwargs in self.__list_messages(ERROR, state,
+                                                         CONV_STATE_TO_CATEGORY.get(conv_state)):
+            bot.send_message(self.chat_id, message_text, **kwargs)
 
 
 def lambda_handler(event: dict, context):
@@ -140,5 +166,9 @@ def lambda_handler(event: dict, context):
     elif update.message.text in conv_handler.get_reply_options(
             DISTANCE) and conv_state == ConversationState.LOCATION_DETAILS:
         conv_handler.process_location_details(state)
+    elif update.message.text in conv_handler.get_reply_options(TIME) and conv_state == ConversationState.TIME:
+        conv_handler.process_time_details(state)
+    else:
+        conv_handler.process_unknown_prompt(conv_state, state)
 
     return {'statusCode': 200}
